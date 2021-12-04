25e1766d440704152cd2af2e447a48bd5b8d7f6c
Karan <karanlvm123@gmail.com>
2021-12-05 02:25:17 +0530

    Unexpected app crash?

--- app/src/main/java/com/example/facedet/MainActivity.java
+++ app/src/main/java/com/example/facedet/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.facedet;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.util.Log;
@@ -15,8 +16,20 @@ import org.opencv.android.BaseLoaderCallback;
 import org.opencv.android.CameraBridgeViewBase;
 import org.opencv.android.JavaCameraView;
 import org.opencv.android.OpenCVLoader;
+import org.opencv.core.Core;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
+import org.opencv.core.MatOfRect;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
+import org.opencv.core.Size;
+import org.opencv.imgproc.Imgproc;
+import org.opencv.objdetect.CascadeClassifier;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2 {
 
@@ -29,6 +42,8 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
     private Mat mRgba;
     private Mat mGray;
 
+    private CascadeClassifier cascadeClassifier;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,9 +53,33 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
         cameraBridgeViewBase = (JavaCameraView)findViewById(R.id.CameraView);
         cameraBridgeViewBase.setVisibility(View.VISIBLE);
         cameraBridgeViewBase.setCvCameraViewListener(this);
+
+        // Loading the model into the application
+        try{
+            InputStream is = getResources().openRawResource(R.raw.haarcascade_frontalface_alt);
+            File cascadeDir = getDir("cascade", Context.MODE_PRIVATE); // Create a folder
+            File mCascadeFile = new File(cascadeDir,"haarcascade_frontalface_alt.xml"); //Creating a new file in that folder
+            FileOutputStream os = new FileOutputStream(mCascadeFile);
+            byte[] buffer = new byte[4096];
+            int byteRead;
+            //writing file from raw folder
+            while((byteRead = is.read(buffer)) != -1){
+                os.write(buffer, 0, byteRead);
+            }
+            is.close();
+            os.close();
+
+            // Load file from the folder created
+            cascadeClassifier = new CascadeClassifier(mCascadeFile.getAbsolutePath());
+        }
+        catch (IOException e){
+            Log.i(TAG, "Cascade file is not found");
+        }
+
+
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
                 != PackageManager.PERMISSION_GRANTED) {
-            // More code here ...
+
         } else {
             Log.d(TAG, "Permissions granted");
             cameraBridgeViewBase.setCameraPermissionGranted();
@@ -75,10 +114,39 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
     @Override
     public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
         mRgba=inputFrame.rgba();
-        mGray=inputFrame.gray();
+        //mGray=inputFrame.gray();
 
+        // We pass mRgba to cascaderec
+
+        mRgba = CascadeRec(mRgba);
         return mRgba;
 
+    }
+
+    private Mat CascadeRec(Mat mRgba) {
+        // Rotate frame by -90 deg
+        Core.flip(mRgba.t(), mRgba, 1);
+        // Convert image to RGB
+        Mat mRbg = new Mat();
+        Imgproc.cvtColor(mRgba, mRbg,Imgproc.COLOR_RGBA2RGB);
+
+        int height = mRbg.height();
+        //Minimum size of face in frame
+        int absoluteFaceSize = (int) (height*0.1);
+
+        MatOfRect faces = new MatOfRect();
+        if(cascadeClassifier !=null){
+            cascadeClassifier.detectMultiScale(mRbg,faces,1.1,2,2, new Size(absoluteFaceSize,absoluteFaceSize), new Size());
+        }
+
+        Rect[] facesArray = faces.toArray();
+        for(int i=0;i<facesArray.length;i++){
+            //Draw bounding box
+            Imgproc.rectangle(mRgba, facesArray[i].tl(),facesArray[i].br(),new Scalar(0,255,0), 2);
+        }
+
+        Core.flip(mRgba.t(), mRgba, 0);
+        return mRgba;
     }
 
     @Override
